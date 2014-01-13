2e2301596d2b0fb761432574638836dd4a3c4c39
Charles Wesley <cwesley@eplicaservices.com>
2014-01-13 12:16:49 -0800

    Bugfix

--- yolk/pypi.py
+++ yolk/pypi.py
@@ -203,13 +203,13 @@ class CheeseShop(object):
         """Return list of pickled package names from PYPI"""
         if self.debug:
             self.logger.debug("DEBUG: reading pickled cache file")
-        return cPickle.load(open(self.pkg_cache_file, "r"))
+        return cPickle.load(open(self.pkg_cache_file, "rb"))
 
     def fetch_pkg_list(self):
         """Fetch and cache master list of package names from PYPI"""
         self.logger.debug("DEBUG: Fetching package name list from PyPI")
         package_list = self.list_packages()
-        cPickle.dump(package_list, open(self.pkg_cache_file, "w"))
+        cPickle.dump(package_list, open(self.pkg_cache_file, "wb"))
         self.pkg_list = package_list
 
     def search(self, spec, operator):
